a549afe9dc738da5648378d022e9cf392729ccda
Robert <73249253+rakkerman-1@users.noreply.github.com>
2024-07-23 22:42:30 -0400

    Add files via upload

---
+++ lab7h.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+Student ID: [seneca_id]
+This object 'a' is accessible by any code in the program:
+
+a = 'object_in_main'
+
+def function1(): # This object 'a' is completely unrelated to the object 'a' in function2(): a = 'object_function1' print('print() call in function1 on a:',a)
+
+def function2(): # This variable 'a' is completely unrelated to the variable 'a' in function1(): a = 'function2_object' print('print() call in function2 on a:',a)
+Note that you cannot access the value of object '''a''' created in function1()
+or function2() with the print() functions after the calling function1() and function2()
+All the print() retrieved the value of object '''a''' defined in the main script.
+
+print('print() call in main on a:',a) function1() print('print() call in main on a:',a) function2() print('print() call in main on a:',a)
